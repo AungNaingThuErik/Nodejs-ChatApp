5245d874da909a41b726ca4a125e6fc1b583fbae
Erik Aung <aungnaingthu@firstcom.com.sg>
2022-07-01 16:37:32 +0800

    added changes

--- app.js
+++ app.js
@@ -18,3 +18,19 @@ app.get('/', (req, res) => {
 const server = app.listen(port, () => {
     console.log(`Server Running on port ${port}`)
 })
+
+//Get username and roomname from form and pass it to room
+app.post('/room', (req, res) => {
+    roomname = req.body.roomname;
+    username = req.body.username;
+    res.redirect(`/room?username=${username}&roomname=${roomname}`)
+})
+
+//Rooms
+app.get('/room', (req, res)=>{
+    res.render('room')
+})
+
+
+const io = socket(server);
+require('./utils/socket')(io);
